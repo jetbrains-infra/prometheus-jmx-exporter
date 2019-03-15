6f87e93f94515e42ab51f15ad9751f7cd3d3858c
Dmitry Shmelev <dmitry.shmelev@jetbrains.com>
2019-03-15 15:06:56 +0300

    Init commit

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+envsubst < /tmp/config.yml.template > /opt/jmx_exporter/config.yml
+
+if [ -z "$SERVICE_PORT" ]; then
+  SERVICE_PORT=5556
+fi
+
+if [ -z "$CONFIG_YML" ]; then
+  CONFIG_YML=/opt/jmx_exporter/config.yml
+fi
+
+java $JVM_OPTS -Djava.util.logging.config.file=/opt/jmx_exporter/logging.properties -jar /opt/jmx_exporter/jmx_prometheus_httpserver-$VERSION-jar-with-dependencies.jar $SERVICE_PORT $CONFIG_YML
